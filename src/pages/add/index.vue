d2b1d1adad082bb776f0b634b6244c9ad52932fe
LJJ <1332680344@qq.com>
2019-12-17 19:55:43 +0800

    seventh

--- src/pages/add/index.vue
+++ src/pages/add/index.vue
@@ -1,30 +1,12 @@
 <template>
-<div>
-<i-panel >
-    <i-input :value="title" type="textarea" mode="wrapped"  placeholder="填写标题会有更多赞哦~" />
-    <i-input :value="key" type="textarea" mode="wrapped" placeholder="为您的分享添加一个关键字吧" />
-    <i-input :value="cntent" type="textarea" mode="wrapped" placeholder="添加正文" i-class="btn" />
-</i-panel>
-<text class='pf-type'>点我添加图片呀~</text>
-    <view class="question-images clearFloat">
-      <block wx:for="images" wx:key="*this">
-        <view class="q-image-wrap">
-          <!-- 图片缩略图  -->
-          <image class="q-image" src="item" mode="aspectFill" data-idx="index" bindtap="handleImagePreview"></image>
-          <!-- 移除图片的按钮  -->
-          <view class="q-image-remover" data-idx="index" bindtap="removeImage">
-            <icon class='iconfont icon-guanbi'></icon>
-          </view>
-        </view>
-      </block>
-      <view class='uploadImgBtn' bindtap="chooseImage" wx:if="images.length < 3">
-        <icon class='iconfont icon-jiahao'></icon>
-      </view>
-    </view>
-    <view><text>\n</text>
-    <i-button @click="handleClick" type="success" shape="circle">发布</i-button>
-    </view>
-</div>
+  <div>
+    <i-panel >
+        <i-input :value="title" type="textarea" mode="wrapped"  placeholder="填写标题会有更多赞哦~" />
+        <i-input :value="key" type="textarea" mode="wrapped" placeholder="为您的分享添加一个关键字吧" />
+        <i-input :value="cntent" type="textarea" mode="wrapped" placeholder="添加正文" i-class="btn" />
+    </i-panel>
+     <i-button @click="handleClick" type="success" shape="circle">发布</i-button>
+  </div>
 </template>
 
 <script>
@@ -34,82 +16,12 @@ export default {
             title:"",
             key:"",
             content:"",
-            images: [],
         }
     },
-    chooseImage(e) {
-    wx.chooseImage({
-      sizeType: ['original', 'compressed'],  //可选择原图或压缩后的图片
-      sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
-      success: res => {
-        const images = this.data.images.concat(res.tempFilePaths)
-        // 限制最多只能留下3张照片
-        const images1 = images.length <= 3 ? images : images.slice(0, 3)
-        this.setData({
-          images: images1
-        })
-      }
-    })
-  },
-  removeImage(e) {
-    var that = this;
-    var images = that.data.images;
-    // 获取要删除的第几张图片的下标
-    const idx = e.currentTarget.dataset.idx
-    // splice  第一个参数是下表值  第二个参数是删除的数量
-    images.splice(idx,1)
-    this.setData({
-      images: images
-    })
-  },
-  handleImagePreview(e) {
-    const idx = e.target.dataset.idx
-    const images = this.data.images
-    wx.previewImage({
-      current: images[idx],  //当前预览的图片
-      urls: images,  //所有要预览的图片
-    })
-  }
 }
+
 </script>
 
 <style scope>
-.q-image-wrap{
-  width: 150rpx;
-  height: 150rpx;
-  border: 3px solid #f0f0f0;
-  float: left;
-  margin-right: 20rpx;
-  position: relative;
-  overflow: hidden;
-}
-.q-image-remover{
-  width: 100rpx;
-  height: 100rpx;
-  border-radius: 100%;
-  background: #2e82ff;
-  position: absolute;
-  top:-50rpx;
-  right:-50rpx;
- 
-}
-.q-image-remover icon{
-  position: absolute;
-  left:12rpx;
-  bottom:12rpx;
-  color:#fff;
-}
- 
-.uploadImgBtn{
-  width: 150rpx;
-  height: 150rpx;
-  text-align: center;
-  line-height: 140rpx;
-  border: 6rpx solid #f0f0f0;
-  float: left;
-}
-.uploadImgBtn icon{
-  font-size: 80rpx;
-  color: #999;
-}
+
 </style>
--- src/pages/homepage/index.vue
+++ src/pages/homepage/index.vue
@@ -22,34 +22,34 @@
   </i-grid>
 
   <i-panel :title="title_name">
-    <block v-for=" item in top" :key="item">
-    <i-card i-class="split" :title="item.name" :thumb="item.image">
-      <view slot="footer">{{item.keyWord}}</view>
-      <view slot="content">{{item.content}}</view>
-    </i-card>
-    <view class="tool">
-  <view class="tool-item" catchtap='onUpTop' data-post-id="post.postId">
-    <image src="/static/images/dianzan.png"></image>
-    <text>{{post.upNum}}</text>
-  </view>
-  <view class="tool-item comment" catchtap='onCommentTap' data-post-id="post.postId">
-    <image src="/static/images/pinlun.png"></image>
-    <text>{{post.commentNum}}</text>
-  </view>
-  <view class="tool-item" @click='on' data-post-id="post.postId">
-   <image v-show="post.collectionStatus" src="/static/images/collect.png"></image>
-   <image v-show="post.collectionStatus2" src="/static/images/_collect.png"></image>
-    <text>{{post.upNum}}</text>
-  </view>
-</view>
+    <block v-for="item in top" :key="item">
+      <i-card i-class="split" :title="item.name" :thumb="item.image">
+        <view slot="footer">{{item.keyWord}}</view>
+        <view slot="content">{{item.content}}</view>
+      </i-card>
+      <view class="tool">
+      <view class="tool-item" @click='onUpTop(item)' data-post-id="post.postId">
+        <image v-show="item.upStatus" src="/static/images/dianzan.png"></image>
+      <image v-show="item.upStatus2" src="/static/images/_dianzan.png"></image>
+      <i-toast id="toast" />
+      </view>
+      <view class="tool-item comment" catchtap='onCommentTap' data-post-id="post.postId">
+        <image src="/static/images/pinlun.png"></image>
+        
+      </view>
+      <view class="tool-item" @click='on(item)' data-post-id="post.postId">
+      <image v-show="item.collectionStatus" src="/static/images/collect.png"></image>
+      <image v-show="item.collectionStatus2" src="/static/images/_collect.png"></image>
+      <i-toast id="toast" />
+      </view>
+    </view>
   </block>
   </i-panel>
   </div>
 </template>
 
 <script>
-
-const { $Toast } = require('../../../static/dist/toast/index');
+ const { $Toast } = require('../../../static/dist/base/index')
 export default {
   data () {
     return {
@@ -61,9 +61,9 @@ export default {
        { title:"攻略",image:"/static/images/4.png"},
      ],
      top:[
-       {name:"武汉之粮道街过早",keyWord:"粮道街 过早",image:"/static/images/liangdaojie.jpg",content:"从去年国庆开始就一直说要去粮道街过早，直到今年的五月份才去，这就是懒惰的女大学生，周六早上八点多起床突发奇想要去，去了也就临近中午了，装作过早的样子，以下几个是我觉得好吃的：爆浆生煎，桂林米粉，七个蛋烘糕，赵师傅油饼包烧麦"},
-       {name:"重庆三天两夜游记",keyWord:"贫民窟女孩 学生党",image:"/static/images/CQ.jpg",content:"由于这是小李同学心血来潮做的一次旅游攻略，做的不好还请大家多多见谅。那么开始喽"},
-       {name:"最全西安攻略",keyWord:"1000块三天三夜玩转西安",image:"/static/images/shanxi.jpg",content:"西安交通 地铁：zfb搜索'西安电子地铁卡'绑定即可 公交：zfb搜索'长安通电子卡'绑定即可"},
+       {name:"武汉之粮道街过早",keyWord:"粮道街 过早",image:"/static/images/liangdaojie.jpg",content:"从去年国庆开始就一直说要去粮道街过早，直到今年的五月份才去，这就是懒惰的女大学生，周六早上八点多起床突发奇想要去，去了也就临近中午了，装作过早的样子，以下几个是我觉得好吃的：爆浆生煎，桂林米粉，七个蛋烘糕，赵师傅油饼包烧麦", upStatus:true,upStatus2: false,collectionStatus: true, collectionStatus2: false,},
+       {name:"重庆三天两夜游记",keyWord:"贫民窟女孩 学生党",image:"/static/images/CQ.jpg",content:"由于这是小李同学心血来潮做的一次旅游攻略，做的不好还请大家多多见谅。那么开始喽", upStatus: false, upStatus2: true,collectionStatus: true,collectionStatus2: false,},
+       {name:"最全西安攻略",keyWord:"1000块三天三夜玩转西安",image:"/static/images/shanxi.jpg",content:"西安交通 地铁：zfb搜索'西安电子地铁卡'绑定即可 公交：zfb搜索'长安通电子卡'绑定即可", upStatus: true,upStatus2: false,collectionStatus: true,collectionStatus2: false,},
      ],
      imgUrls:[
        '/static/images/wuhan1.jpg',
@@ -74,12 +74,9 @@ export default {
      autoplay:false,
      interval:5000,
      duration:1000,
-     post: {
-       collectionStatus: true,
-       collectionStatus2: false,
-     }
     }
   },
+  
   methods: {
    goType(type) {
      console.log(type)
@@ -87,7 +84,7 @@ export default {
      mpvue.navigateTo({ url })
    },
    //更新本地的点赞、评论信息、收藏、阅读量
-updatePostData(category){
+  updatePostData(category){
     var itemData = this.getPostItemById(),
      postData = itemData.data,
     allPostData = this.getAllPostData();
@@ -113,35 +110,50 @@ updatePostData(category){
               postData.upStatus = false;
             }
             break;
-          
         default:break;
     }
     //更新缓存数据库  
     allPostData[itemData.index] = postData;
     this.execSetStorageSync(allPostData);
     return postData;
-},   
+  },   
     collect(){
       return this.updatePostData('collect');
     },
 
-  on() {
-      this.post.collectionStatus = !this.post.collectionStatus;
-      this.post.collectionStatus2 = !this.post.collectionStatus2;
-     
-      this.onLoad();
+  on(item) {
+      item.collectionStatus = !item.collectionStatus;
+      item.collectionStatus2 = !item.collectionStatus2;
+      this.handleMask(item);
   },
-
-  
-  onLoad() {
-      wx.showToast({
-     title:newData.collectionStatus?"收藏成功":"收藏取消",
-    //  duration:1000,
-    //  icon:"sucess",
-     make:true
-     })
-  }
-  }
+  handleMask (item) {
+    $Toast({
+        content:item.collectionStatus? '取消收藏':'收藏成功',
+        type: 'success',
+        duration: 0,
+        mask: false
+    });
+    setTimeout(() => {
+        $Toast.hide();
+    }, 5000);
+  },
+  onUpTop(item) {
+       item.upStatus = !item.upStatus;
+       item.upStatus2 = !item.upStatus2;
+       this.handleMask2(item);
+    },
+  handleMask2 (item) {
+    $Toast({
+        content:item.upStatus? '取消点赞':'点赞成功',
+        type: 'success',
+        duration: 0,
+        mask: false
+    });
+    setTimeout(() => {
+        $Toast.hide();
+    }, 5000);
+  },
+}
 }
 </script>
 
@@ -166,6 +178,26 @@ updatePostData(category){
 .comment image{
   transform: scale(.85);
 }
+.toast {
+            position: fixed;
+            z-index: 2000;
+            left: 50%;
+            top:60%;
+            transition:all .5s;
+            -webkit-transform: translateX(-50%) translateY(-50%);
+               -moz-transform: translateX(-50%) translateY(-50%);
+                -ms-transform: translateX(-50%) translateY(-50%);
+                 -o-transform: translateX(-50%) translateY(-50%);
+                    transform: translateX(-50%) translateY(-50%);
+            text-align: center;
+            border-radius: 5px;
+            color:#FFF;
+            background: rgba(17, 17, 17, 0.7);
+            height: 45px;
+            line-height: 45px;
+            padding: 0 15px;
+          
+        }
 div >>> .no-border {
   border-width: 0pt;
 }
